f3fd9d472153e31af38c4c98867abe9eb53c1eb7
tkdalsss <lsm991229@naver.com>
2023-04-07 13:14:30 +0900

    study week 10-02(2)

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 set -e 
 
 echo "run db migration"
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 echo "start the app"
 exec "$@" # takes all parameters passed to the script and run it
